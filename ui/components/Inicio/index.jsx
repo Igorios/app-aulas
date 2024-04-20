dd63101ad513afdebc1232a9310d43cf74f2e453
Igor Borges <borgesigor153@gmail.com>
2024-04-20 12:25:20 -0300

    :sparkles: feat: listando na tela os alunos

--- ui/components/Inicio/index.jsx
+++ ui/components/Inicio/index.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { useAlunos } from "../../../data/hooks/alunos";
 
 export default function Inicio() {
-
   const { alunos, getAllAlunos } = useAlunos();
+  console.log(alunos);
 
   useEffect(() => {
     const fetchData = async () => {
-        await getAllAlunos();
-    };re
+      await getAllAlunos();
+    };
     fetchData();
-    
-}, []);
-
+  }, []);
 
   return (
     <>
-      <Text className="text-red-600 text-3xl">Teste do app</Text>
-      <Text className="text-red-600 mt-2">Instalando o applicativo com react native!!!</Text>
+    <View className="">
+
+      {alunos.map((aluno) => (
+        <View key={aluno.idAluno} className="mb-5">
+          <Text>
+            <Text> Nome: <Text className="font-bold">{aluno.nomeAluno}</Text></Text>
+            <Text> Nascido(a) em: <Text className="font-bold">{aluno.nascimento}</Text></Text>
+          </Text>
+          <View className="border-b-2 border-gray-200" />
+        </View>
+      ))}
+
+    </View>
     </>
   );
 }
