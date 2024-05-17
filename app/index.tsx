626d9846757e59d1c928cf5c5b101b0befc29eba
Igor Borges <borgesigor153@gmail.com>
2024-05-16 23:56:51 -0300

    :sparkles: feat: nova rota para cadstro de alunos

--- app/index.tsx
+++ app/index.tsx
@@ -16,6 +16,9 @@ export default function App() {
           <Link className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-3xl text-center text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-44 p-3" href="/screens/alunos/todosAlunos">
             <Text>Visualizar os Alunos <AntDesign name="arrowright" color="white" /></Text>
           </Link>
+          <Link className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-3xl text-center text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-44 p-3" href="/screens/(tabs)">
+            <Text>Painel administrativo <AntDesign name="arrowright" color="white" /></Text>
+          </Link>
         </View>
 
       </View>
---
+++ app/screens/(tabs)/dashboard/criarAluno/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+
+export default function CriarAluno() {
+    return<>
+        <Text>Criar um aluno!</Text>
+    </>
+}
