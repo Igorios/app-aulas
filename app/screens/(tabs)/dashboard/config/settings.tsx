db303a5c14f366760de7383caf761ce64ecd7186
Igor Borges <borgesigor153@gmail.com>
2024-05-19 11:48:54 -0300

    :lipstick: feat: feat: estilizando a interface de configuracao

--- app/screens/(tabs)/dashboard/config/settings.tsx
+++ app/screens/(tabs)/dashboard/config/settings.tsx
@@ -1,12 +1,16 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Settings() {
 
     return(
-        <View className="p-10">
-            <Text className="mb-5">Configuração do arquivo config do APP</Text>
-            <Link href={"/"}>Volte para a página HOME do APP</Link>
+        <View className="p-10 bg-gray-200 h-screen">
+            <Text className="mb-5 text-lg">Configuração do APP</Text>
+            <Link className="text-lg" href={"/"}>
+                Voltar para a página inicial {" "}
+                 <FontAwesome color={"rgb(31 41 55)"} name="home" size={25}   />
+            </Link>
         </View>
     );
 
